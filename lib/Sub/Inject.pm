1b65a7324c1968e2abfdf376ae50b3a7377e52c1
Adriano Ferreira <a.r.ferreira@gmail.com>
2017-09-10 15:15:08 -0700

    Initial Sub::Inject - Perl code

---
+++ lib/Sub/Inject.pm
@@ -0,0 +1,19 @@
+
+package Sub::Inject;
+
+# ABSTRACT: Inject subroutines into a lexical scope
+
+use 5.018;
+
+require XSLoader;
+XSLoader::load(__PACKAGE__);
+
+1;
+
+=encoding utf8
+
+=head1 NAME
+
+Sub::Inject - Inject subroutines into a lexical scope
+
+=cut
